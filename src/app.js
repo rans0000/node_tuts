2604ff946962ee90f1cf2b9b816b391bc032f6a2
rans0000 <rans0000@gmail.com>
2022-09-14 00:35:29 +0530

    add express js

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile('/view/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('/view/about.html', { root: __dirname });
+});
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile('/view/404.html', { root: __dirname });
+});

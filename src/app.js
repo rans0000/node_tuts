939eb85d65b52fe6513ee4a2abf892408d0d034f
rans0000 <rans0000@gmail.com>
2022-09-15 20:39:15 +0530

    add public folder for style

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 app.listen(3000);
 
+app.use(express.static('./src/public'));
 app.get('/', (req, res) => {
     res.sendFile('/view/index.html', { root: __dirname });
 });

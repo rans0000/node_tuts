9732b70735843750b44b9a72fe3bb8c72072c4a0
rans0000 <rans0000@gmail.com>
2022-09-15 21:17:52 +0530

    add ejs template engine and use render in express

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
 app.listen(3000);
 
 app.use(express.static('./src/public'));
 app.get('/', (req, res) => {
-    res.sendFile('/views/index.html', { root: __dirname });
+    res.render('index', { title: 'Home' });
+    // res.sendFile('/views/index.html', { root: __dirname });
 });
-
 app.get('/about', (req, res) => {
-    res.sendFile('/views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
+    // res.sendFile('/views/about.html', { root: __dirname });
 });
-
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create Blog' });
 });
-
 app.use((req, res) => {
-    res.status(404).sendFile('/views/404.html', { root: __dirname });
+    res.render('404', { title: '404' });
+    // res.status(404).sendFile('/views/404.html', { root: __dirname });
 });
